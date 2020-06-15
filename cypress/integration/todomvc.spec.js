7c0167df1e78a574d0bb131cfc9b30deb20daad9
Santosh Marigowda <santosharakere@gmail.com>
2020-06-15 20:56:35 +0100

    end of section

--- cypress/integration/todomvc.spec.js
+++ cypress/integration/todomvc.spec.js
@@ -3,6 +3,10 @@
 it("Should be able to add a new To Do to the list", function () {
   cy.visit("http://todomvc-app-for-testing.surge.sh");
   cy.get(".new-todo", { timeout: 6000 }).type("Learn Cypress").type("{enter}");
+  cy.get('label').should('have.text', 'Learn Cypress');
+  cy.get('input.toggle').should('not.be.checked');
   cy.get("input.toggle").click();
+  cy.get('label').should('have.css', 'text-decoration-line', 'line-through');
   cy.contains('Clear completed').click();
+  cy.get('ul.todo-list').should('not.have.descendants', 'li');
 });
