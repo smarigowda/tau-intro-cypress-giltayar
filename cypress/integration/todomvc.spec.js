d59c4c735a34ab5b881373ca83012ee72e1d4d7c
Santosh Marigowda <santosharakere@gmail.com>
2020-06-15 16:29:30 +0100

    end of section commit

--- cypress/integration/todomvc.spec.js
+++ cypress/integration/todomvc.spec.js
@@ -1,6 +1,8 @@
 /// <reference types="cypress" />
 
 it("Should be able to add a new To Do to the list", function () {
-  cy.visit("http://todomvc-app-for-testing.surge.sh?delay-new-todo=5000");
+  cy.visit("http://todomvc-app-for-testing.surge.sh");
   cy.get(".new-todo", { timeout: 6000 }).type("Learn Cypress").type("{enter}");
+  cy.get("input.toggle").click();
+  cy.contains('Clear completed').click();
 });
