d21f23cbd06b53f86dba6d55a58f07790650d05d
Santosh Marigowda <santosharakere@gmail.com>
2020-06-15 22:08:16 +0100

    splitting into 3 tests

--- cypress/integration/todomvc.spec.js
+++ cypress/integration/todomvc.spec.js
@@ -1,12 +1,18 @@
 /// <reference types="cypress" />
 
-it("Should be able to add a new To Do to the list", function () {
+it("Should add a new to do to the list", function () {
   cy.visit("http://todomvc-app-for-testing.surge.sh");
   cy.get(".new-todo", { timeout: 6000 }).type("Learn Cypress").type("{enter}");
-  cy.get('label').should('have.text', 'Learn Cypress');
-  cy.get('input.toggle').should('not.be.checked');
+  cy.get("label").should("have.text", "Learn Cypress");
+  cy.get("input.toggle").should("not.be.checked");
+});
+
+it("Should mark a to do as completed", function () {
   cy.get("input.toggle").click();
-  cy.get('label').should('have.css', 'text-decoration-line', 'line-through');
-  cy.contains('Clear completed').click();
-  cy.get('ul.todo-list').should('not.have.descendants', 'li');
+  cy.get("label").should("have.css", "text-decoration-line", "line-through");
+});
+
+it("Should clear completed to dos", function () {
+  cy.contains("Clear completed").click();
+  cy.get("ul.todo-list").should("not.have.descendants", "li");
 });
