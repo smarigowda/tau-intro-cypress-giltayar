89dabf302f775df5f2d115218694c932d318baff
Santosh Marigowda <santosharakere@gmail.com>
2020-06-15 11:38:20 +0100

    delay 3 sec, test passes

--- cypress/integration/todomvc.spec.js
+++ cypress/integration/todomvc.spec.js
@@ -1,6 +1,6 @@
 /// <reference types="cypress" />
 
 it("Should be able to add a new To Do to the list", function () {
-  cy.visit("http://todomvc-app-for-testing.surge.sh/");
+  cy.visit("http://todomvc-app-for-testing.surge.sh?delay-new-todo=3000");
   cy.get(".new-todo").type("Learn Cypress").type("{enter}");
 });
