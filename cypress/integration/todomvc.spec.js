bd0f842dc262dfa14dc468449c2e8b5960165c50
Santosh Marigowda <santosharakere@gmail.com>
2020-06-15 11:47:01 +0100

    default timeout increased by passing an option to the command

--- cypress/integration/todomvc.spec.js
+++ cypress/integration/todomvc.spec.js
@@ -2,5 +2,5 @@
 
 it("Should be able to add a new To Do to the list", function () {
   cy.visit("http://todomvc-app-for-testing.surge.sh?delay-new-todo=5000");
-  cy.get(".new-todo").type("Learn Cypress").type("{enter}");
+  cy.get(".new-todo", { timeout: 6000 }).type("Learn Cypress").type("{enter}");
 });
