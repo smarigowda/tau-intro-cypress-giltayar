0a16bb761968b02d518d72b0ac26eafff39310d3
Santosh Marigowda <santosharakere@gmail.com>
2020-06-17 09:30:38 +0100

    error resolved itself, test results not appearing in applitools

--- cypress/integration/todomvc-visual.spec.js
+++ cypress/integration/todomvc-visual.spec.js
@@ -1,10 +1,20 @@
 /// <reference types="cypress" />
 
+import TodoPage from "../page-objects/todopage";
+
+let todoPage;
+
 describe("Visual Spec", function () {
   before("open the app", function () {
     cy.visit("http://todomvc-app-for-testing.surge.sh");
+    cy.eyesOpen({ appName: "Cypress Course", batchName: "Cypress Course" });
+    todoPage = new TodoPage();
   });
-  it('Should do the visual checking of empty to do', function() {
-      console.log('place holder now');
-  })
+  after("After block", function () {
+    cy.eyesClose();
+  });
+  it("Should do the visual checking", function () {
+    todoPage.addToDo("Hello welcome to Cypress");
+    cy.eyesCheckWindow("One to do added");
+  });
 });
