de6e715f590f61421ea781df518693737d817996
Santosh Marigowda <santosharakere@gmail.com>
2020-06-17 05:24:45 +0100

    renamed object

--- cypress/integration/todomvc-actions.spec.js
+++ cypress/integration/todomvc-actions.spec.js
@@ -2,25 +2,25 @@
 
 import TodoPage from "../page-objects/todopage";
 
-let todo = new TodoPage();
+let todoPage = new TodoPage();
 
 describe("to do actions", function () {
   beforeEach("setup for each test", function () {
-    todo
+    todoPage
       .navigateTo("http://todomvc-app-for-testing.surge.sh")
       .addToDo("Clean the house")
       .addToDo("Learn Cypress");
   });
   it("Should add a new to do to the list", function () {
-    todo.setTodoIndexTo(0).verifyText("Learn Cypress").shouldNotBeChecked();
+    todoPage.setTodoIndexTo(0).verifyText("Learn Cypress").shouldNotBeChecked();
   });
 
   it("Should mark a to do as completed", function () {
-    todo.setTodoIndexTo(0).toggle().shouldHaveLineThrough();
+    todoPage.setTodoIndexTo(0).toggle().shouldHaveLineThrough();
   });
 
   it("Should mark the to do as not completed", function () {
-    todo
+    todoPage
       .setTodoIndexTo(0)
       .toggle()
       .shouldHaveLineThrough()
@@ -29,7 +29,15 @@ describe("to do actions", function () {
   });
 
   it("Should clear completed to dos", function () {
-    todo.setTodoIndexTo(0).toggle().clearCompleted().verifyTodoItemsCount(1);
-    todo.setTodoIndexTo(0).toggle().clearCompleted().verifyTodoItemsCount(0);
+    todoPage
+      .setTodoIndexTo(0)
+      .toggle()
+      .clearCompleted()
+      .verifyTodoItemsCount(1);
+    todoPage
+      .setTodoIndexTo(0)
+      .toggle()
+      .clearCompleted()
+      .verifyTodoItemsCount(0);
   });
 });
